5de45aae5a6dcfab901eb8fac39d026fef55fe34
Aman Jain <amanjain7838@gmail.com>
2020-10-02 22:16:04 +0530

    functionality version 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, faCheckSquare, faCoffee)
 
 ReactDOM.render(
   <React.StrictMode>
