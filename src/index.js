7d2f648e0f3e957ca25a96da2d17497959a34f5b
Aman Jain <amanjain7838@gmail.com>
2020-10-10 16:07:08 +0530

    sentry added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-library.add(fab, faCheckSquare, faCoffee)
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fab } from '@fortawesome/free-brands-svg-icons';
+// import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+// library.add(fab, faCheckSquare, faCoffee)
+Sentry.init({
+  dsn: "https://efbb2ce497ee4ffd8112a25f28ff6c17@o459682.ingest.sentry.io/5459162",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
