c962138d9149e829848fd802a2bee609d4faa35c
Aman Jain <amanjain7838@gmail.com>
2021-02-21 21:35:35 +0530

    Updates

---
+++ precache-manifest.f8dc2f78770e1329b984e94ebb31485d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b4389f92b68584b9176fe2e42f48e8",
+    "url": "/myscreenrecord/index.html"
+  },
+  {
+    "revision": "77ec622256038eda79c0",
+    "url": "/myscreenrecord/static/css/2.ab33dd16.chunk.css"
+  },
+  {
+    "revision": "d82dbb074376e9487f7a",
+    "url": "/myscreenrecord/static/css/main.5781d6d9.chunk.css"
+  },
+  {
+    "revision": "77ec622256038eda79c0",
+    "url": "/myscreenrecord/static/js/2.91dc7d39.chunk.js"
+  },
+  {
+    "revision": "91d6ace9d027cfdf1468acde7d1a7615",
+    "url": "/myscreenrecord/static/js/2.91dc7d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82dbb074376e9487f7a",
+    "url": "/myscreenrecord/static/js/main.60d5ab02.chunk.js"
+  },
+  {
+    "revision": "65fcdbdb9344f18aee8a",
+    "url": "/myscreenrecord/static/js/runtime-main.1ea28548.js"
+  }
+]);
